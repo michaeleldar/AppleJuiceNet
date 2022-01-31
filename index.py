29218eea7dc9b428a37aa86f22b78842a1b3cdf4
Michael Halpin <halpinm2010@gmail.com>
2022-01-31 16:38:07 +0000

    .

--- index.py
+++ index.py
@@ -1,3 +1,14 @@
+import socket
 assert url.startswith("http://")
 url = url[len("http://"):]
 host, path = url.split("/", 1)
+path = "/" + path
+
+s = socket.socket(
+    family=socket.AF_INET,
+    type=socket.SOCK_STREAM,
+    proto=socket.IPPROTO_TCP,
+)
+
+s.connect(("example.org", 80))
+s.send(b"GET /index.html HTTP/1.0\r\n" + b"Host: example.org\r\n\r\n")
