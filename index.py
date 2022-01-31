d2ea8cafd3bb24df2b71d1ce97c29cddb3d9badd
Michael Halpin <halpinm2010@gmail.com>
2022-01-31 17:59:33 +0000

    .

--- index.py
+++ index.py
@@ -10,23 +10,24 @@ s = socket.socket(
     type=socket.SOCK_STREAM,
     proto=socket.IPPROTO_TCP,
 )
-s.connect(("example.org", 80))
-s.send(b"GET /index.html HTTP/1.0\r\n" + b"Host: example.org\r\n\r\n")
-response = s.makefile("r", encoding="utf8", newline="\r\n")
-statusline = response.readline()
-version, status, explanation = statusline.split(" ", 2)
-assert status == "200", "{}: {}".format(status, explanation)
-headers = {}
-while True:
-    line = response.readline()
-    if line == "\r\n": break
-    header, value = line.split(":", 1)
-    headers[header.lower()] = value.strip()
-
-body = response.read()
-s.close
 
 def request(url):
+    s.connect(("example.org", 80))
+    s.send(b"GET /index.html HTTP/1.0\r\n" + b"Host: example.org\r\n\r\n")
+    response = s.makefile("r", encoding="utf8", newline="\r\n")
+    statusline = response.readline()
+    version, status, explanation = statusline.split(" ", 2)
+    assert status == "200", "{}: {}".format(status, explanation)
+    headers = {}
+    while True:
+        line = response.readline()
+        if line == "\r\n": break
+        header, value = line.split(":", 1)
+        headers[header.lower()] = value.strip()
+
+    body = response.read()
+    s.close
+
     return headers, body
 
 def show(body):
